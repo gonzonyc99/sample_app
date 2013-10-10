7b3073863e12943738d5655c6cd899ac9ef2ec58
Ariel Gonzalez <gonzoucf@gmail.com>
2013-10-10 16:24:01 -0400

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,7 +8,7 @@ describe "Static pages" do
     before { visit root_path }
 
     it { should have_content('Sample App') }
-    it { should have_title("Ruby on Rails Tutorial Sample App") }
+    it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
 end
 
@@ -16,31 +16,35 @@ end
     before { visit help_path }
 
     it { should have_content('Help') }
-    it { should have_title("Ruby on Rails Tutorial Sample App | Help") }
+    it { should have_title(full_title('Help')) }
 end
   
   describe "About page" do
     before { visit about_path }
 
     it { should have_content('About Us') }
-    it { should have_title("Ruby on Rails Tutorial Sample App | About Us") }
+    it { should have_title(full_title('About Us')) }
   end
 
     describe "Contact page" do
     before { visit contact_path }
 
     it { should have_content('Contact') }
-    it { should have_title("Ruby on Rails Tutorial Sample App | Contact") }
+    it { should have_title(full_title('Contact')) }
 end
 
     it "should have the right links on the layout" do
       visit root_path
-      click_link "About Us"
-      expect(page).to have_title(full_title('About Us'))
+      click_link "About"
+      expect(page).to have_title('About Us')
       click_link "Help"
-      expect(page).to have_title(full_title('Help'))
+      expect(page).to have_title('Help')
       click_link "Contact"
-      expect(page).to have_title(full_title('Contact'))
+      expect(page).to have_title('Contact')
       click_link "Home"
+      click_link "sample app"
+      expect(page).to have_title('Sample App')
+      click_link "Sign up now!"
+      expect(page).to have_title('Sign up')
     end
 end
